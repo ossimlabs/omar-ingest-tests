38602d68c5eb3478ff05de5aa4394fcef0b480c0
Benjamin <40779150+benbuzzelli@users.noreply.github.com>
2019-07-09 09:46:23 -0400

    Update and rename 04_Local_Staging_Service.feature to 04_Local_Staging_Service.f

--- src/cucumber/resources/features/04_Local_Staging_Service.f
+++ src/cucumber/resources/features/04_Local_Staging_Service.f
@@ -1,5 +1,5 @@
-# @local_staging_service
-# Feature: StagingService
+@local_staging_service
+Feature: StagingService
     Scenario: [DSTG-01] Make a local hsi envi image available for discovery
         Given a local hsi envi image is not already indexed
         When a local hsi envi image is indexed into OMAR
